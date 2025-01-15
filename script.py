7add42d81b614e3abf8c33504f01b8a04f5cf6c0
deepanjali kumari <deepanjali.kumari@iitgn.ac.in>
2025-01-15 19:40:15 +0530

    Add script.py with sample code

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import math
 
 def area_of_square(side):
-  return side*side
+    return side * side
 
 def area_of_rectangle(length, width):
     return length * width
@@ -13,40 +13,40 @@ def area_of_triangle(base, height):
     return 0.5 * base * height
 
 def main():
-    print("select shape:")
-    print("1.square")
+    print("Select shape:")
+    print("1. Square")
     print("2. Rectangle")
     print("3. Circle")
     print("4. Triangle")
     print("5. Exit")
-
+    
     while True:
         choice = input("\nEnter shape (1-4): ")
-        
-        if choice=="1":
-            side = float(input("input side of square: "))
+    
+        if choice == "1":
+            side = float(input("Input side of square: "))
             area = area_of_square(side)
-            print(f"area of the square: {area:.2f}")
+            print(f"Area of the square: {area:.2f}")
 
         elif choice == "2":
-            length = float(input("input length of rectangle: "))
-            width = float(input("width of the rectangle: "))
+            length = float(input("Input length of rectangle: "))
+            width = float(input("Input width of the rectangle: "))
             area = area_of_rectangle(length, width)
-            print(f"area of the rectangle: {area:.2f}")
+            print(f"Area of the rectangle: {area:.2f}")
 
         elif choice == "3":
-            radius = float(input("input radius of circle: "))
+            radius = float(input("Input radius of circle: "))
             area = area_of_circle(radius)
-            print(f"area of the circle: {area:.2f}")
+            print(f"Area of the circle: {area:.2f}")
 
         elif choice == "4":
-            base = float(input("input base of triangle: "))
-            height = float(input("input height of triangle: "))
+            base = float(input("Input base of triangle: "))
+            height = float(input("Input height of triangle: "))
             area = area_of_triangle(base, height)
-            print(f"area of the triangle: {area:.2f}")
+            print(f"Area of the triangle: {area:.2f}")
 
         elif choice == "5":
-            print("exit!")
+            print("Exit!")
             break
 
         else:
